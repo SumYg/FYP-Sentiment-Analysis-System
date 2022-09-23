8c7146b2aacdd8413ce66ef6c174ca0f536972a3
SumYg <61381909+SumYg@users.noreply.github.com>
2022-09-24 00:45:46 +0800

    Basic functions for trends and twitter api

---
+++ google_trends.py
@@ -0,0 +1,51 @@
+from pytrends.request import TrendReq
+# from pprint import pprint
+# import logging
+# pytrend = TrendReq(hl='en-US', tz=360)
+# keywords = ['intel', 'amd', 'samsung', 'apple', 'tesla', '4']
+# pytrend.build_payload(
+#      kw_list=keywords,
+#      cat=0,
+#      timeframe='today 1-m',
+#      geo='HK',
+#      gprop='')
+
+# pprint(pytrend.interest_over_time())
+
+# pytrends = TrendReq(hl='en-US', tz=360)
+
+# pprint(pytrends.trending_searches(pn='hong_kong'))
+# print("=================")
+
+class GoogleTrends():
+    def __init__(self) -> None:
+        self.pytrend = TrendReq(hl='en-US', tz=360)
+
+    def get_trending_searches(self, pn='united_states'):
+        """
+        Return the dataframe which contains 20 trending searches.
+        A bit different with the site
+        https://trends.google.com/trends/trendingsearches/daily?geo=US
+        """
+        # pytrend = TrendReq(hl='en-US', tz=360)
+
+        trending_searches = self.pytrend.trending_searches(pn=pn)
+        # logging.info(trending_searches)
+        print(trending_searches)
+        # pprint(pytrend.realtime_trending_searches(pn='US'))
+        return trending_searches
+
+
+    def get_suggestions(self, keyword):
+        """
+        Get related keywords
+        """
+        # pytrend = TrendReq(hl='en-US', tz=360)
+        suggestions = self.pytrend.suggestions(keyword)
+        print(suggestions)
+        return suggestions
+
+if __name__ == '__main__':
+    # get_trending_searches()
+    google_trends = GoogleTrends()
+    google_trends.get_suggestions("Alex")
